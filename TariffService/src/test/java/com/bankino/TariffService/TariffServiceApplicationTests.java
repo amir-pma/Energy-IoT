c8a878c4ff5c45ac1f54f75ff6cedb57c695bc7c
amir pma <amir.pma1378@gmail.com>
2021-08-09 16:19:12 +0430

    added tariff service project

---
+++ TariffService/src/test/java/com/bankino/TariffService/TariffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankino.TariffService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TariffServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
