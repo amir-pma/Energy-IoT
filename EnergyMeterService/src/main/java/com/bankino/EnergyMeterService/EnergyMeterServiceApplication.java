d33f2f7b729666d0ff5669d746e97a4a6ce8eb30
amir pma <amir.pma1378@gmail.com>
2021-08-09 16:15:46 +0430

    added eneregy meter service project

---
+++ EnergyMeterService/src/main/java/com/bankino/EnergyMeterService/EnergyMeterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bankino.EnergyMeterService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyMeterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyMeterServiceApplication.class, args);
+	}
+
+}
