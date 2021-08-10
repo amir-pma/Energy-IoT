c00693fde7637bfc3a611393ffb00481eff09426
amir pma <amir.pma1378@gmail.com>
2021-08-10 04:42:53 +0430

    added controller and service packages and configured rest template

--- EnergyMeterService/src/main/java/com/bankino/EnergyMeterService/service/EnergyMeterService.java
+++ EnergyMeterService/src/main/java/com/bankino/EnergyMeterService/service/EnergyMeterService.java
@@ -1,4 +1,23 @@
 package com.bankino.EnergyMeterService.service;
 
+import com.bankino.EnergyMeterService.repository.EnergyMeterRepository;
+import com.bankino.EnergyMeterService.repository.MeterDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.client.RestTemplate;
+
 public class EnergyMeterService {
+
+    @Autowired
+    @Lazy
+    private RestTemplate template;
+
+    private final EnergyMeterRepository energyMeterRepository;
+    private final MeterDataRepository meterDataRepository;
+
+
+    public EnergyMeterService(EnergyMeterRepository energyMeterRepository, MeterDataRepository meterDataRepository) {
+        this.energyMeterRepository = energyMeterRepository;
+        this.meterDataRepository = meterDataRepository;
+    }
 }
