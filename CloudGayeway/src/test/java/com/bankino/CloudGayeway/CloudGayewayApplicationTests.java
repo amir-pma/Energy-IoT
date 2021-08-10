bc1a5fdbfb5dedd829a9b7ca1755acd3c6ebde33
amir pma <amir.pma1378@gmail.com>
2021-08-10 20:49:12 +0430

    added cloud gateway project

---
+++ CloudGayeway/src/test/java/com/bankino/CloudGayeway/CloudGayewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankino.CloudGayeway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGayewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
