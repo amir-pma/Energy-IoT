6cc8812c228e26c88990458b57f14328e0d4939e
amir pma <amir.pma1378@gmail.com>
2021-08-11 01:17:14 +0430

    added hystrix dashboard configuration

--- CloudGayeway/src/main/java/com/bankino/CloudGayeway/CloudGatewayApplication.java
+++ CloudGayeway/src/main/java/com/bankino/CloudGayeway/CloudGatewayApplication.java
@@ -3,13 +3,15 @@ package com.bankino.CloudGayeway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CloudGayewayApplication {
+@EnableHystrix
+public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudGayewayApplication.class, args);
+		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
 }
--- Hystrix-Dashboard/src/main/java/com/bankino/HystrixDashboard/HystrixDashboardApplication.java
+++ Hystrix-Dashboard/src/main/java/com/bankino/HystrixDashboard/HystrixDashboardApplication.java
@@ -2,8 +2,11 @@ package com.bankino.HystrixDashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
