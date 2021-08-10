226e7e9cd050e8d2f77a6bab133b9991cde1b6dc
amir pma <amir.pma1378@gmail.com>
2021-08-10 04:32:46 +0430

    added dtos

---
+++ EnergyMeterService/src/main/java/com/bankino/EnergyMeterService/dto/MeterDataDTO.java
@@ -0,0 +1,12 @@
+package com.bankino.EnergyMeterService.dto;
+
+import java.sql.Timestamp;
+
+public class MeterDataDTO {
+
+    private Long id;
+    private Timestamp timestamp;
+    private double consumptionKWH;
+    private Long EnergyMeterId;
+
+}
