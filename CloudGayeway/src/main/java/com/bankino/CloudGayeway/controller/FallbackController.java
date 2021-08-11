3ed6a85cc227c0782f3c52b8ac0a154c439dfddd
amir pma <amir.pma1378@gmail.com>
2021-08-11 17:18:49 +0430

    added meter data message producer service config in gateway

--- CloudGayeway/src/main/java/com/bankino/CloudGayeway/controller/FallbackController.java
+++ CloudGayeway/src/main/java/com/bankino/CloudGayeway/controller/FallbackController.java
@@ -7,13 +7,18 @@ import reactor.core.publisher.Mono;
 @RestController
 public class FallbackController {
 
+    @RequestMapping("/meterDataMessageProducerFallback")
+    public Mono<String> meterDataMessageProducerFallback() {
+        return Mono.just("Meter Data Message Producer is taking too long to respond! Please try again later!");
+    }
+
     @RequestMapping("/energyMeterFallback")
-    public Mono<String> EnergyMeterServiceFallback() {
+    public Mono<String> energyMeterServiceFallback() {
         return Mono.just("Energy meter service is taking too long to respond! Please try again later!");
     }
 
     @RequestMapping("/tariffFallback")
-    public Mono<String> TariffServiceFallback() {
+    public Mono<String> tariffServiceFallback() {
         return Mono.just("Tariff service is taking too long to respond! Please try again later!");
     }
 }
