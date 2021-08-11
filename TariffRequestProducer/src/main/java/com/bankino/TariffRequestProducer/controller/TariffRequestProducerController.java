7bb2fef7360e30b79cd32fb38f57cbb037615998
amir pma <amir.pma1378@gmail.com>
2021-08-11 15:18:57 +0430

    added tariff request producer controller

--- TariffRequestProducer/src/main/java/com/bankino/TariffRequestProducer/controller/TariffRequestProducerController.java
+++ TariffRequestProducer/src/main/java/com/bankino/TariffRequestProducer/controller/TariffRequestProducerController.java
@@ -13,5 +13,22 @@ import org.springframework.cloud.stream.messaging.Source;
 @RestController
 public class TariffRequestProducerController {
 
-   
+    private final Source source;
+
+    private final String prefix;
+
+    public TariffRequestProducerController(Source source, @Value("${http.prefix:''}") String prefix) {
+        this.source = source;
+        this.prefix = prefix;
+    }
+
+    @RequestMapping("/send-message")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void sendMessage(@RequestBody String message) {
+        source.output().
+            send(MessageBuilder.
+                withPayload(String.join("|", prefix, message)).
+                build()
+            );
+    }
 }
