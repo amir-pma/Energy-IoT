7272447faabbace86b8c82e17d6fff2c3e72f709
amir pma <amir.pma1378@gmail.com>
2021-08-11 19:23:32 +0430

    added alert service

--- AlertService/src/main/java/com/bankino/AlertService/AlertService/AlertService.java
+++ AlertService/src/main/java/com/bankino/AlertService/AlertService/AlertService.java
@@ -2,6 +2,9 @@ package com.bankino.AlertService.AlertService;
 
 import com.bankino.AlertService.config.EmailConfig;
 import com.bankino.AlertService.model.Alert;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
@@ -10,5 +13,33 @@ import javax.validation.ValidationException;
 @Service
 public class AlertService {
 
-   
+    private final EmailConfig emailConfig;
+
+    private static String FROM_EMAIL = "energy_iot_service@gmail.com";
+
+    public AlertService(EmailConfig emailConfig) {
+        this.emailConfig = emailConfig;
+    }
+
+    public void sendMail(Alert alert, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            throw new ValidationException("Alert email is wrong or empty");
+        }
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(emailConfig.getHost());
+        mailSender.setPort(emailConfig.getPort());
+        mailSender.setUsername(emailConfig.getUsername());
+        mailSender.setPassword(emailConfig.getPassword());
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(FROM_EMAIL);
+        mailMessage.setTo(alert.getToEmail());
+        mailMessage.setSubject(alert.getMailSubject());
+        mailMessage.setText(alert.getMailText());
+
+        mailSender.send(mailMessage);
+
+    }
 }
