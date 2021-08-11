f0a08b8552c1aca41ffcb833f05330dde3f89fe2
amir pma <amir.pma1378@gmail.com>
2021-08-11 15:17:57 +0430

    added kafka config

--- TariffRequestProducer/src/main/java/com/bankino/TariffRequestProducer/TariffRequestProducerApplication.java
+++ TariffRequestProducer/src/main/java/com/bankino/TariffRequestProducer/TariffRequestProducerApplication.java
@@ -2,8 +2,14 @@ package com.bankino.TariffRequestProducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableBinding(Source.class)
 public class TariffRequestProducerApplication {
 
 	public static void main(String[] args) {
---
+++ TariffRequestProducer/src/main/java/com/bankino/TariffRequestProducer/controller/TariffRequestProducerController.java
@@ -0,0 +1,17 @@
+package com.bankino.TariffRequestProducer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.stream.messaging.Source;
+
+
+@RestController
+public class TariffRequestProducerController {
+
+   
+}
