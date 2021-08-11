026a4caaee66358dbd9ba6ec2bbfb1df2b1f9d39
amir pma <amir.pma1378@gmail.com>
2021-08-11 14:58:38 +0430

    added energy meter request producer project

---
+++ EnergyMeterRequestProducer/src/main/java/com/bankino/EnergyMeterRequestProducer/EnergyMeterRequestProducerApplication.java
@@ -0,0 +1,13 @@
+package com.bankino.EnergyMeterRequestProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyMeterRequestProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyMeterRequestProducerApplication.class, args);
+	}
+
+}
---
+++ EnergyMeterRequestProducer/src/test/java/com/bankino/EnergyMeterRequestProducer/EnergyMeterRequestProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankino.EnergyMeterRequestProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyMeterRequestProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
