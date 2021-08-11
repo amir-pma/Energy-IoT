89364355f43f0b03cf682628fad9b7b742bcca1b
amir pma <amir.pma1378@gmail.com>
2021-08-11 14:59:11 +0430

    added tariff request producer project

---
+++ TariffRequestProducer/src/main/java/com/bankino/TariffRequestProducer/TariffRequestProducerApplication.java
@@ -0,0 +1,13 @@
+package com.bankino.TariffRequestProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffRequestProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffRequestProducerApplication.class, args);
+	}
+
+}
---
+++ TariffRequestProducer/src/test/java/com/bankino/TariffRequestProducer/TariffRequestProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankino.TariffRequestProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TariffRequestProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
