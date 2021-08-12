eec64bff798ed58d97d5d449ae387dc024be0cc2
amir pma <amir.pma1378@gmail.com>
2021-08-12 19:38:50 +0430

    added swagger doc for gateway service

--- CloudGayeway/src/main/java/com/bankino/CloudGayeway/CloudGatewayApplication.java
+++ CloudGayeway/src/main/java/com/bankino/CloudGayeway/CloudGatewayApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableSwagger2
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
--- CloudGayeway/src/main/java/com/bankino/CloudGayeway/controller/FallbackController.java
+++ CloudGayeway/src/main/java/com/bankino/CloudGayeway/controller/FallbackController.java
@@ -1,23 +1,29 @@
 package com.bankino.CloudGayeway.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Api(description = "REST API for handling fallback addresses of services.")
 public class FallbackController {
 
     @RequestMapping("/meterDataMessageProducerFallback")
+    @ApiOperation(value = "Fallback for message producer service.")
     public Mono<String> meterDataMessageProducerFallback() {
         return Mono.just("Meter Data Message Producer is taking too long to respond! Please try again later!");
     }
 
     @RequestMapping("/energyMeterFallback")
+    @ApiOperation(value = "Fallback for energy meter service.")
     public Mono<String> energyMeterServiceFallback() {
         return Mono.just("Energy meter service is taking too long to respond! Please try again later!");
     }
 
     @RequestMapping("/tariffFallback")
+    @ApiOperation(value = "Fallback for tariff service.")
     public Mono<String> tariffServiceFallback() {
         return Mono.just("Tariff service is taking too long to respond! Please try again later!");
     }
